361686111b36036e608c994d86958147676f9b26
Bart Enkelaar <benkelaar@gmail.com>
2020-12-01 21:31:51 +0100

    Improve performance of solution

--- src/main/kotlin/com/bartenkelaar/expenses/SumChecker.kt
+++ src/main/kotlin/com/bartenkelaar/expenses/SumChecker.kt
@@ -4,12 +4,24 @@ class SumChecker {
     fun findPairSum(entries: Set<Int>) = findPairSum(entries, 2020)
 
     private fun findPairSum(entries: Set<Int>, target: Int): Pair<Int, Int>? {
-        for (entry in entries) {
-            val partner = target - entry
-            if (partner != entry && partner in entries) return entry to partner
+        val sorted = entries.sorted()
+
+        tailrec fun findPairRecursive(lowIndex: Int, highIndex: Int): Pair<Int, Int>? {
+            if (lowIndex == highIndex) return null
+            val low = sorted[lowIndex]
+            val high = sorted[highIndex]
+            val sum = low + high
+
+            return if (sum == target) (low to high)
+            else findPairRecursive(
+                lowIndex = if (sum < target) lowIndex + 1 else lowIndex,
+                highIndex = if (sum > target ) highIndex - 1 else highIndex,
+            )
         }
-        return null;
+
+        return findPairRecursive(0, entries.size - 1);
     }
+
 
     fun findTripleSum(entries: Set<Int>): List<Int> {
         for (entry in entries) {
