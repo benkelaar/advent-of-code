b0d25ac30a1a0b07a54ffb10c1564cdb40a8fe11
Bart Enkelaar <benkelaar@gmail.com>
2020-12-03 15:31:00 +0100

    Small renaming

--- src/main/kotlin/com/bartenkelaar/year2015/packaging/WrappingCalculator.kt
+++ src/main/kotlin/com/bartenkelaar/year2015/packaging/WrappingCalculator.kt
@@ -4,24 +4,24 @@ import com.bartenkelaar.Solver
 
 class WrappingCalculator : Solver {
     override fun solve(input: List<String>): Pair<Int, Int> {
-        val dimensions = input.map { Dimension.forString(it) }
+        val dimensions = input.map { Package.forString(it) }
         return dimensions.map { it.paperRequired() }.sum() to
                 dimensions.map { it.ribbonRequired() }.sum()
     }
 }
 
-data class Dimension(
-    val low: Int,
-    val mid: Int,
-    val high: Int,
+data class Package(
+    val lowDim: Int,
+    val midDim: Int,
+    val highDim: Int,
 ) {
-    fun paperRequired() = 3 * low * mid + 2 * (low * high + mid * high)
-    fun ribbonRequired() = 2 * (low + mid) + low * mid * high
+    fun paperRequired() = 3 * lowDim * midDim + 2 * (lowDim * highDim + midDim * highDim)
+    fun ribbonRequired() = 2 * (lowDim + midDim) + lowDim * midDim * highDim
 
     companion object {
-        fun forString(str: String): Dimension {
+        fun forString(str: String): Package {
             val (low, mid, high) = str.trim().split("x").map { it.toInt() }.sorted()
-            return Dimension(low, mid, high)
+            return Package(low, mid, high)
         }
     }
 }
--- src/test/kotlin/com/bartenkelaar/year2015/packaging/PackageTest.kt
+++ src/test/kotlin/com/bartenkelaar/year2015/packaging/PackageTest.kt
@@ -3,14 +3,14 @@ package com.bartenkelaar.year2015.packaging
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class DimensionTest {
+class PackageTest {
     @Test
     fun `given dimension string, extract proper dimensions`() {
-        assertEquals(Dimension.forString("1x3x2"), Dimension(1, 2, 3))
+        assertEquals(Package.forString("1x3x2"), Package(1, 2, 3))
     }
 
     @Test
     fun `given dimensions, calculate paper required`() {
-        assertEquals(24, Dimension(1, 2, 3).paperRequired())
+        assertEquals(24, Package(1, 2, 3).paperRequired())
     }
 }
